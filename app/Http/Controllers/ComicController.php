92a9a3dda72ab1eb3330ae199101b9c3b9cc1a27
MarcoBeraldo <beraldomar@gmail.com>
2022-09-21 16:53:13 +0200

    created model and controller

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Comic;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('home', compact('comics'));
+
+    }
+
+}

43c2323909226a5efabfa986fb69d73a10b62745
MarcoBeraldo <beraldomar@gmail.com>
2022-09-22 19:58:11 +0200

    added navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
     <title>Comics</title>
 </head>
 <body>
-<header>    
-    <a href="{{route('comics.index')}}">Homepage fumetti</a>
-    <a href="{{route('comics.create')}}">Nuovo fumetto</a>
-</header>
+@include('includes.header')
 
     <main>
         <div>

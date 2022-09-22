501776d7307c244251ed8b1f6bf2f124a36e1b3f
MarcoBeraldo <beraldomar@gmail.com>
2022-09-22 16:54:53 +0200

    created migration

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,15 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Comic;
-
 class ComicController extends Controller
 {
-    public function index()
-    {
-        $comics = Comic::all();
-        return view('home', compact('comics'));
-
-    }
-
+    //
 }
